c5d3582a8cb812d91e640f3c1a8503c4a1981e6b
Perrix0815 <moritzmliwa@gmail.com>
2022-12-05 19:44:31 +0100

    Add project files.

---
+++ ClassLibrary1/KnifeDamageMod.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace KnifeDamageMod_SN
+{
+    class KnifeDamageMod
+    {
+        [HarmonyPatch(typeof(PlayerTool))]
+        [HarmonyPatch("Awake")]
+        internal class PatchPlayerToolAwake
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerTool __instance)
+            {
+
+                if (__instance.GetType() == typeof(Knife))
+                {
+                    Knife knife = __instance as Knife;
+
+                    // Double the knife damage
+                    float knifeDamage = knife.damage;
+                    float newKnifeDamage = knifeDamage * 2;
+                    knife.damage = newKnifeDamage;
+                    Logger.Log(Logger.Level.Debug, $"Knife damage was: {knifeDamage}," +
+                        $" is now: {newKnifeDamage}");
+                }
+            }
+        }
+    }
+}
